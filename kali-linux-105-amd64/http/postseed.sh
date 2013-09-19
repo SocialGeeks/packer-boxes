f7feca528bdb25c54d01f3bc655084bf60722e74
Dan Daggett <ddaggett@kickbackpoints.com>
2013-09-19 11:49:00 -0600

    Finish kali installation scripts. Organize kali post install scripts into modules.

--- kali-linux-105-amd64/http/postseed.sh
+++ kali-linux-105-amd64/http/postseed.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
-apt-get install sudo
-sed -i -e 's/\%sudo[\t]ALL=(ALL:ALL) ALL//\%sudo[\t]ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
+apt-get install -y sudo
+
+cp /etc/sudoers /etc/sudoers.bak
+
+cat <<EOF > /etc/sudoers
+Defaults	env_reset
+Defaults	mail_badpass
+Defaults	secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+
+root	ALL=(ALL:ALL) ALL
+%sudo	ALL=(ALL) NOPASSWD: ALL
+EOF
 
 update-rc.d ssh enable
 
--- kali-linux-105-amd64/scripts/ssh.sh
+++ kali-linux-105-amd64/scripts/ssh.sh
@@ -3,21 +3,8 @@
 # Most of this script was lifted from https://github.com/jedi4ever/veewee/tree/master/templates/archlinux-x86_64
 # and changed to work with packer with my own customizations
 
-echo 'nameserver 8.8.8.8' > /etc/resolv.conf
-
 # Disable SSH root login
 sed -i -e 's/\PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
-
-# Install and set up VirtualBox Guest Additions
-apt-get update && apt-get upgrade
-apt-get install -y linux-headers-$(uname -r)
-mkdir vbox
-mount VBoxGuestAdditions.iso vbox/
-cd vbox
-./VBoxLinuxAdditions.run
-cd ..
-umount vbox
-rm -rf vbox VBoxGuestAdditions.iso
 
 # Turn on X Forwarding
 sed -i -e 's/\#X11Forwarding no/X11Forwarding yes/' /etc/ssh/sshd_config
@@ -25,19 +12,6 @@ sed -i -e 's/\#X11DisplayOffset/X11DisplayOffset/' /etc/ssh/sshd_config
 sed -i -e 's/\#X11UseLocalhost/X11UseLocalhost/' /etc/ssh/sshd_config
 sed -i -e 's/\#AllowTcpForwarding/AllowTcpForwarding/' /etc/ssh/sshd_config
 
-# Setup vagrant user
-useradd -m -G sudo,vboxsf -r vagrant
-passwd -d vagrant
-passwd vagrant<<EOF
-vagrant
-vagrant
-EOF
-chsh -s /bin/bash vagrant
-cd /home/vagrant
-mkdir -m 700 .ssh
-wget https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub
-mv vagrant.pub .ssh/authorized_keys
-chmod 600 .ssh/authorized_keys
-chown -R vagrant:vagrant .ssh
+# Turn on public key authentication
 sed -i -e 's/\\#Pub/Pub/g' /etc/ssh/sshd_config
 
