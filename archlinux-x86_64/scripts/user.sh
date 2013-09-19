c46a12c7b25f6abda2df0c481b0e815b1c256681
Dan Daggett <ddaggett@kickbackpoints.com>
2013-09-19 13:46:35 -0600

    Organize archlinux post install scripts into modules.

---
+++ archlinux-x86_64/scripts/user.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Most of this script was lifted from https://github.com/jedi4ever/veewee/tree/master/templates/archlinux-x86_64
+# and changed to work with packer with my own customizations
+
+passwd -l root
+
+# Setup vagrant user
+pacman --noconfirm -S bash wget vim
+useradd -m -G wheel,vboxsf -r vagrant
+chsh -s /bin/bash vagrant
+cd /home/vagrant
+mkdir -m 700 .ssh
+wget https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub
+mv vagrant.pub .ssh/authorized_keys
+chmod 600 .ssh/authorized_keys
+chown -R vagrant:vagrant .ssh
+
